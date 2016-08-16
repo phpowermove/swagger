bbb97b5267fa0c582ea87465a4d72d6e873a5036
jacobemerick <jpemeric@gmail.com>
2016-08-15 18:14:28 -0700

    Adds description to security scheme

--- src/SecurityScheme.php
+++ src/SecurityScheme.php
@@ -11,16 +11,30 @@ class SecurityScheme extends AbstractModel implements Arrayable {
 	/** @var string */
 	private $type;
 
+	/** @var string */
+	private $description;
+
+	/**
+	 * @param string $name
+	 * @param array $contents
+	 */
 	public function __construct($name, $contents = []) {
 		$this->name = $name;
 		$this->parse($contents);
 	}
 
+	/**
+	 * @param array $contents
+	 */
 	private function parse($contents = []) {
 		$this->type = $contents->get('type');
+		$this->description = $contents->get('description');
 	}
 
+	/**
+	 * @return array
+	 */
 	public function toArray() {
-		return $this->export('type');
+		return $this->export('type', 'description');
 	}
 }
